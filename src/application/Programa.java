8bbd6f3b767aa3d218794e228673f4a5e0067107
Adeilsoara <adeilsonaragao@hotmail.com>
2020-03-31 08:51:42 -0300

    Classe Departamento

---
+++ src/application/Programa.java
@@ -0,0 +1,13 @@
+package application;
+
+import model.entities.Departamento;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		
+		Departamento obj = new Departamento(1, "TI");
+		System.out.println(obj);
+	}
+
+}
