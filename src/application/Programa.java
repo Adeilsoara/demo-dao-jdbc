15957836149d3ca8cb32967e470a9fe606715b0a
Adeilsoara <adeilsonaragao@hotmail.com>
2020-03-31 09:11:00 -0300

    Classe Vendedor

--- src/application/Programa.java
+++ src/application/Programa.java
@@ -1,13 +1,19 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Departamento;
+import model.entities.Vendedor;
 
 public class Programa {
 
 	public static void main(String[] args) {
 		
 		Departamento obj = new Departamento(1, "TI");
-		System.out.println(obj);
+		
+		Vendedor ven = new Vendedor(1, "Adeilson", "adeilsonaragao@gmail.com", new Date(), 10000.00, obj);
+		
+		System.out.println(ven);
 	}
 
 }
